bf8dea1491b5e38710b05292cc6827d77f714c51
Raghavendra-PS <raghavendra_ps@icloud.com>
2025-04-17 17:31:12 +0530

    v2.7

--- filter_engine.py
+++ filter_engine.py
@@ -9,7 +9,13 @@ from typing import List, Dict, Optional, Any
 # --- Constants ---
 # (Keep constants as before)
 TENDER_BLOCK_PATTERN = re.compile(r"^\d+\.\s*$")
-INDIAN_STATES = [ "Andhra Pradesh", "Arunachal Pradesh", # ... (full list) ...
+INDIAN_STATES = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
+    "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka",
+    "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya",
+    "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim",
+    "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand",
+    "West Bengal", "Delhi", "Jammu and Kashmir", "Ladakh", "Puducherry",
+    "Chandigarh", "Andaman and Nicobar Islands", "Dadra and Nagar Haveli and Daman and Diu", "Lakshadweep"
 ]
 TWO_BRACKET_ID_REGEX = re.compile(r"^\s*\[[^\]]+\]\s*\[(\d{4}_\w+_\d+_\d+)\]\s*$")
 TAG_REGEX = {
